92f3d0db6cd41e9eb5b9e95aa2ce00ae0e345d99
radarbogor <radarbogor.dev@gmail.com>
2023-10-28 12:58:20 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,12 +4,12 @@
     <div class="container text-md-left">
       <div class="row text-md-left pt-3">
         <div class="col-md-6">
-          <img src="{{ asset('img/dpkLogo.png') }}" alt="logo radar bogor" class="mb-2" style="width: 250px">
+          <img src="{{ asset('img/dpkLogo.png') }}" alt="logo radar bogor" class="mb-2" style="width: 200px">
           <hr>
-          <h5>Graha Pena Bogor</h5>
+          <h5>Radar Depok</h5>
           <div class="d-flex">
             <i class="fas fa-map-marker-alt mt-1"></i>
-            <p class="ms-3">Jl KHR Abdullah Bin Nuh Jl. Ring Road Taman Yasmin No.30, RT.05/RW.04, Cibadak, Kec. Bogor Barat, Kota Bogor, Jawa Barat 16113.</p>
+            <p class="ms-3">Ruko Verbena Boulevard Grand Depok City Depok 16421</p>
           </div>
         </div>
 
@@ -17,28 +17,23 @@
           <h5 class="text-uppercase mb-4 fw-bold">Social</h5>
           <hr>
           <p>
-            <a class="text-decoration-none text-light" href="https://www.youtube.com/channel/UCZ0d_a44D-aORvJze6V-0Kg" target="_blank">
+            <a class="text-decoration-none text-light" href="https://www.youtube.com/channel/UCBGRhD-CAfakv7IEKO6GSxw" target="_blank">
               <i class="fab fa-youtube me-3"></i>Youtube
             </a>
           </p>
           <p>
-            <a class="text-decoration-none text-light" href="https://www.tiktok.com/@radarbogor" target="_blank">
+            <a class="text-decoration-none text-light" href="https://www.tiktok.com/@radardepok.id" target="_blank">
               <i class="fab fa-tiktok me-3"></i>Tiktok
             </a>
           </p>
           <p>
-            <a class="text-decoration-none text-light" href="https://www.instagram.com/radar_bogor" target="_blank">
+            <a class="text-decoration-none text-light" href="https://www.instagram.com/radar.depok/" target="_blank">
               <i class="fab fa-instagram me-3"></i>Instagram
             </a>
           </p>
           <p>
-            <a class="text-decoration-none text-light" href="https://www.facebook.com/radar.bogor" target="_blank">
+            <a class="text-decoration-none text-light" href="https://www.facebook.com/profile.php?id=100085030903186&mibextid=ZbWKwL" target="_blank">
               <i class="fab fa-facebook me-3"></i>Facebook
-            </a>
-          </p>
-          <p>
-            <a class="text-decoration-none text-light" href="https://twitter.com/radarbogorID" target="_blank">
-              <i class="fab fa-twitter me-3"></i>Twitter
             </a>
           </p>
         </div>
@@ -47,13 +42,10 @@
           <h5 class="text-uppercase mb-4 fw-bold">Contact</h5>
           <hr>
           <p>
-            <i class="fas fa-envelope me-3"></i>redaksi@radar-bogor.com
+            <i class="fas fa-envelope me-3"></i>redaksiradardepok@gmail.com
           </p>
           <p>
-            <i class="fas fa-phone me-3"></i>0251 754 4001
-          </p>
-          <p>
-            <i class="fab fa-whatsapp me-3"></i>0815 8302 200
+            <i class="fas fa-phone me-3"></i>021-7702570
           </p>
         </div>
         <hr>
